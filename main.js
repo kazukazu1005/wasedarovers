9d63a6e65be64e1c5e7be17d29b72d37d9b7a952
kazukazu1005 <uikazu327@gmail.com>
2024-12-18 19:11:26 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const questions = document.querySelectorAll('.quest');
+const answers = document.querySelectorAll('.answer');
+const q = document.querySelectorAll('.q');
+questions.forEach((question, index) => {
+    question.addEventListener('click', () => {
+        const isCurrentlyShown = answers[index].classList.contains('show');
+
+        answers.forEach(answer => answer.classList.remove('show'));
+        q.forEach(q => q.classList.remove('active')); 
+
+        if (!isCurrentlyShown) {
+            answers[index].classList.add('show');
+            q[index].classList.add('active');
+        }
+    });
+});
